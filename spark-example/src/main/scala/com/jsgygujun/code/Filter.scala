de74a010b585a73f3ef9a9bd7dea364e92c9dd73
jun <jsgygujun@gmail.com>
2020-08-02 22:58:30 +0800

    [ADD] ADD Filter

---
+++ spark-example/src/main/scala/com/jsgygujun/code/Filter.scala
@@ -0,0 +1,14 @@
+package com.jsgygujun.code
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Filter {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("MapPartitions").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val list1 = List(30, 50, 70, 60, 10, 20)
+    val rdd1 = sc.parallelize(list1, 2)
+    val rdd2 = rdd1.filter(_ > 20)
+    rdd2.collect().foreach(println)
+  }
+}
