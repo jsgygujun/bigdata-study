31e361efaac02da46d05dc60fc5f40ed276cc47e
jun <jsgygujun@gmail.com>
2020-08-02 22:31:34 +0800

    [ADD] ADD MapPartitionsWithIndex

---
+++ spark-example/src/main/scala/com/jsgygujun/code/MapPartitionsWithIndex.scala
@@ -0,0 +1,17 @@
+package com.jsgygujun.code
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object MapPartitionsWithIndex {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("MapPartitionsWithIndex").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val list1 = List(30, 50, 70, 60, 10, 20)
+    val rdd1 = sc.parallelize(list1, 2)
+    //mapPartitionsWithIndex: 带分区号
+    val rdd2 = rdd1.mapPartitionsWithIndex((index, it) => {
+      it.map((index, _))
+    })
+    rdd2.collect().foreach(println)
+  }
+}
