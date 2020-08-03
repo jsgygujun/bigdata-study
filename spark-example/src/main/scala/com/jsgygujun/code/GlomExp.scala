1579392a2a853d27d678f2be071c9f9438491b64
jun <jsgygujun@gmail.com>
2020-08-03 21:00:06 +0800

    [UPDATE] GlomExp

--- spark-example/src/main/scala/com/jsgygujun/code/GlomExp.scala
+++ spark-example/src/main/scala/com/jsgygujun/code/GlomExp.scala
@@ -13,13 +13,11 @@ object GlomExp {
     val sc = new SparkContext(conf)
     val list = List(1,2,3,4,5,6,7,8,9)
     val rdd1 = sc.parallelize(list, 2)
-    // 将每一个分区的元素合并成一个数组
+    // 将每一个分区的元素合并成一个数组，一个分区一个数组
     val rdd2 = rdd1.glom()
-    val array = rdd2.collect()
-    array.foreach(it => {
-      print("p: ")
-      it.foreach(x => print(x + " "))
-      println()
-    })
+    val rdd3 = rdd2.map(_.toList) // 数组转List
+    val array = rdd3.collect()
+    array.foreach(println)
+    sc.stop()
   }
 }
