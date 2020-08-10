2325c44bd88310687b8d208e44fef4e8435831ec
jun <jsgygujun@gmail.com>
2020-08-10 23:25:43 +0800

    [ADD] GlomExp

---
+++ spark/example/spark-core-example/src/main/scala/com/jsgygujun/code/spark_core/transformation/GlomExp.scala
@@ -0,0 +1,19 @@
+package com.jsgygujun.code.spark_core.transformation
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object GlomExp {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local[*]").setAppName("FlatMapExp")
+    val sc = new SparkContext(conf)
+    glomDemo(sc)
+    sc.stop()
+  }
+
+  def glomDemo(sc: SparkContext): Unit ={
+    val list = List(1, 3, 5, 7, 9, 2, 4, 6, 8)
+    val rdd1 = sc.parallelize(list, 4)
+    val rdd2 = rdd1.glom().map(_.toList)
+    rdd2.collect.foreach(println)
+  }
+}
