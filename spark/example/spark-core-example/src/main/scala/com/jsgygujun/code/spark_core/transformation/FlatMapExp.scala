5734f3b58814cbca758ff0f290fb50f75529f6ba
jun <jsgygujun@gmail.com>
2020-08-10 23:22:12 +0800

    [ADD] FlatMapExp

---
+++ spark/example/spark-core-example/src/main/scala/com/jsgygujun/code/spark_core/transformation/FlatMapExp.scala
@@ -0,0 +1,23 @@
+package com.jsgygujun.code.spark_core.transformation
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object FlatMapExp {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local[*]").setAppName("FlatMapExp")
+    val sc = new SparkContext(conf)
+    flatMapDemo(sc)
+    sc.stop()
+  }
+
+  /**
+   * flatMap
+   * @param sc
+   */
+  def flatMapDemo(sc: SparkContext): Unit = {
+    val list = List(1,3,5,7,9,2,4,6,8)
+    val rdd1 = sc.parallelize(list, 4)
+    val rdd2 = rdd1.flatMap(x => if (x % 2 == 0) List(x, x * x, x * x * x) else List[Int]())
+    rdd2.collect.foreach(println)
+  }
+}
--- spark/example/spark-core-example/src/main/scala/com/jsgygujun/code/spark_core/transformation/MapExp.scala
+++ spark/example/spark-core-example/src/main/scala/com/jsgygujun/code/spark_core/transformation/MapExp.scala
@@ -1,4 +1,4 @@
-package com.jsgygujun.code.spark_core
+package com.jsgygujun.code.spark_core.transformation
 
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -17,10 +17,11 @@ object MapExp {
    * 针对集合中的每个元素执行一次传递的函数
    * 一进一出
    * 将来用来做数据结构的调整
+   *
    * @param sc
    */
   def mapDemo(sc: SparkContext): Unit = {
-    val list = List(1,3,5,7,9,2,4,6,8)
+    val list = List(1, 3, 5, 7, 9, 2, 4, 6, 8)
     val rdd1 = sc.parallelize(list)
     val rdd2 = rdd1.map(_ * 2)
     rdd2.collect.foreach(println)
@@ -30,10 +31,11 @@ object MapExp {
    * mapPartitions
    * 传递的函数, 每个分区执行一次
    * 有可能会出现oom
+   *
    * @param sc
    */
   def mapPartitionsDemo(sc: SparkContext): Unit = {
-    val list = List(1,3,5,7,9,2,4,6,8)
+    val list = List(1, 3, 5, 7, 9, 2, 4, 6, 8)
     val rdd1 = sc.parallelize(list)
     val rdd2 = rdd1.mapPartitions(it => {
       // it 是一个Iterator类型
@@ -45,10 +47,11 @@ object MapExp {
   /**
    * mapPartitionsWithIndex
    * 与mapPartitions一样，只是多个一个分区号
+   *
    * @param sc
    */
   def mapPartitionsWithIndexDemo(sc: SparkContext): Unit = {
-    val list = List(1,3,5,7,9,2,4,6,8)
+    val list = List(1, 3, 5, 7, 9, 2, 4, 6, 8)
     val rdd1 = sc.parallelize(list, 4)
     val rdd2 = rdd1.mapPartitionsWithIndex((index, it) => {
       // index, 分区号，it 是一个Iterator类型
